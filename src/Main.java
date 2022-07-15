effefa8f1a0a66b961d5555a268e56c2146394c2
Jessica <jsouzasouza120@gmail.com>
2022-07-14 22:13:54 -0300

    Exemplo de sobrecarga de métodos

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(6);
+        Quadrilatero.area(4d,4d);
+        Quadrilatero.area(6,7,8);
+        Quadrilatero.area(9f,9f);
+
+    }
+}
